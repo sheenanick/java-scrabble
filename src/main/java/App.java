c69cd80b084bac1f0b34acd6fe086186561cd612
Sheena Nickerson <sheenanick@gmail.com>
2016-09-07 14:35:21 -0700

    add user interface

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,11 @@
+import java.io.Console;
+
 public class App {
   public static void main(String[] args) {
-    System.out.println ("Hello World");
+    Console console = System.console();
+    System.out.println ("Enter a word to check its Scrabble score.");
+    String word = console.readLine();
+    Scrabble scrabble = new Scrabble();
+    System.out.println("The scrabble score for '" + word + "' is " + scrabble.calculateScore(word) + ".");
   }
 }
